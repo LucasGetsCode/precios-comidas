6b4dde11f9046e45805c661eaa16830e2f9f1b7a
Nico Pujía <nicobatman0@gmail.com>
2023-05-31 15:23:54 -0300

    actualización clase, movido a función

--- busqueda_general.py
+++ busqueda_general.py
@@ -1,5 +1,7 @@
 url = 'https://www.carrefour.com.ar/bebidas/gaseosas/gaseosas-cola?fuzzy=0&map=category-1,category-2,category-3,brand&operator=and&order=&page=1&query=/bebidas/gaseosas/gaseosas-cola/coca-cola' 
-clase = 'lyracons-dynamic-weight-price-0-x-currencyContainer'
+# clase = 'lyracons-dynamic-weight-price-0-x-currencyContainer'
+clase_container = "valtech-carrefourar-dynamic-weight-price-0-x-currencyContainer" # Puede que vayan cambiando los nombres de la clase
+clase_precio = "valtech-carrefourar-dynamic-weight-price-0-x-currencyInteger"
 
 #importing required libraries
 from selenium import webdriver
@@ -21,26 +23,23 @@ options.add_argument("--log-level=3") # Evita que se vean los mensajes de la con
 # options.add_argument("--enable-javascript")
 # options.add_argument('--no-sandbox')
 # options.add_argument('--disable-gpu')
-with webdriver.Chrome('chromedriver', options=options) as driver:
 
-	# url = 'https://www.doordash.com/en-US'
-	# driver.maximize_window() #maximize the window
-	# driver.set_window_size(1024, 768)
-	# driver.set_window_position(0, 0) # Top left
+def carrefour_general(driver, url):
+	precios = []
 	driver.get(url)          #open the URL
 	driver.implicitly_wait(15) #maximum time to load the link
 	print("Página cargada, esperando a que carguen los artículos")
 	driver.execute_script("window.scrollTo(0, document.body.scrollHeight,)") # Mueve la ventana para que se carguen bien los elementos
 
 	message = "Oops, se nos acabó el tiempo"
-	email = WebDriverWait(driver, 30, ignored_exceptions=[TimeoutException]).until(EC.visibility_of_element_located((By.CLASS_NAME, clase)), message=message)
+	email = WebDriverWait(driver, 30, ignored_exceptions=[TimeoutException]).until(EC.visibility_of_element_located((By.CLASS_NAME, clase_container)), message=message)
 	segundos = 2
 	print(f"Ya se cargó el primero, esperando {segundos} segundos para repetir")
 	for i in range(segundos):
 		time.sleep(1)
 		print(segundos-1-i) # Está repetido porque por alguna razón desaparece al encontrarlo por primera vez
 
-	email = WebDriverWait(driver, 30, ignored_exceptions=[TimeoutException]).until(EC.visibility_of_element_located((By.CLASS_NAME, clase)), message=message)
+	email = WebDriverWait(driver, 30, ignored_exceptions=[TimeoutException]).until(EC.visibility_of_element_located((By.CLASS_NAME, clase_container)), message=message)
 	segundos = 3
 	print(f"Ya se volvió a cargar uno, esperando {segundos} segundos")
 	for i in range(segundos):
@@ -49,11 +48,10 @@ with webdriver.Chrome('chromedriver', options=options) as driver:
 	print("Productos cargados")
 	result = driver.page_source
 	soup = BeautifulSoup(result, 'html.parser')
-	page = list(soup.findAll('span', class_=clase)) # Span del tipo $ 512,32, separado -$- -512-,-32-
-	precios = []
+	page = list(soup.findAll('span', class_=clase_container)) # Span del tipo $ 512,32, separado -$- -512-,-32-
 	for span in page:
 		print(span.text)
-		precios.append(int(span.find('span', class_="lyracons-dynamic-weight-price-0-x-currencyInteger").text)) # Span de tipo entero
+		precios.append(int(span.find('span', class_=clase_precio).text)) # Span de tipo entero
 	if len(precios) != 0:
 		print(precios.sort())
 		promedio = statistics.mean(precios)
@@ -67,3 +65,10 @@ with webdriver.Chrome('chromedriver', options=options) as driver:
 	print(f"{len(page)} artículos encontrados")
 	print("Listo")
 	# driver.quit()
+
+with webdriver.Chrome('chromedriver', options=options) as driver:
+	carrefour_general(driver, url)
+	carrefour_general(driver, "https://www.carrefour.com.ar/manzana?_q=manzana&map=ft")
+	# driver.maximize_window() #maximize the window
+	# driver.set_window_size(1024, 768)
+	# driver.set_window_position(0, 0) # Top left
